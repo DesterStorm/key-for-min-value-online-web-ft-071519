5dfdecfffefff9c9d4a30722461edba7dc690f14
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-27 02:26:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,3 @@
-# prereqs: iterators, hashes, conditional logic
-
-
 # Given a hash with numeric values, return the key for the smallest value
 # Build a method key_for_min_value that accepts an argument of a hash. This method should iterate
 # over the hash and return the key (not the value!) that points to the smallest value of the set. If
